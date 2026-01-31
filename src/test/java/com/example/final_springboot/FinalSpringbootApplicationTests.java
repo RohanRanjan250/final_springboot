07bc2bda391f7fe6919875d659021dd0b028f9c7
RohanRanjan250 <ranjanrohan250@gmail.com>
2026-02-01 00:48:54 +0530

    chore: Add test files and git attributes

---
+++ src/test/java/com/example/final_springboot/FinalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
